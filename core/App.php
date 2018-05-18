601438b3d6b2c72c5ed7a270d339527f049c8f06
Artyom Andreevich <lutschenkokras@psu.ru>
2018-05-19 00:54:58 +0500

    Создание точки входа и конфигурационного файла

--- core/App.php
+++ core/App.php
@@ -7,12 +7,19 @@ define('COMPONENTS_DIR', ROOT . '/components/');
 define('CORE_DIR', ROOT . '/core/');
 define('METHODS_DIR', ROOT . '/methods/');
 define('MODELS_DIR', ROOT . '/models/');
+define('CONFIG_DIR', CORE_DIR . 'config/');
 define('PHP_EXT', '.php');
 
+/**
+ * Class App
+ * @package core
+ */
 class App
 {
     /** @var  App */
     protected static $_instance = null;
+    /** @var array  */
+    protected static $_config = [];
 
     /**
      * @return App
@@ -29,5 +36,15 @@ class App
      * App constructor.
      */
     private function __construct() {
+    }
+
+    public function run($config) {
+        $fileNmeConfig = CONFIG_DIR . $config . PHP_EXT;
+
+        if (!file_exists($fileNmeConfig)) {
+            throw new \Exception('Not found config file');
+        }
+
+        self::$_config = include ($fileNmeConfig);
     }
 }
--- index.php
+++ index.php
@@ -4,3 +4,5 @@ define('ROOT', dirname(__FILE__));
 
 require_once ROOT . '/core/Autoloader.php';
 \core\Autoloader::register();
+
+\core\App::getInstance()->run('web');
